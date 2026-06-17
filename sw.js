fc642900c25089fc438225116ffbd3a11fbcb690
resiliencyhq-design <resiliencyhq@gmail.com>
2026-06-17 11:38:29 +1000

    Polish Player screen layout and hierarchy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="pitchiq-sprint-4-v1";
+const CACHE="pitchiq-sprint-5-5-player-compact";
 const ASSETS=["./","./index.html","./css/style.css","./css/tokens.css","./js/app/main.js","./js/app/routes.js","./js/app/uxScreens.js","./assets/brand/logo.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))));
