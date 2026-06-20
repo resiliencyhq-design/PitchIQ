5b7d29a91d4ee6d4b057b59809acb6ddeaed2327
resiliencyhq-design <resiliencyhq@gmail.com>
2026-06-20 19:29:20 +1000

    Cache bust landing logo update

--- js/app/main.js
+++ js/app/main.js
@@ -6,7 +6,7 @@ import { REWARDS } from "../data/rewards.js";
 import { PitchIQCameraEngine } from "../services/camera.js";
 import { PitchIQVoiceEngine } from "../services/voice.js";
 import { toast, sparkles } from "../components/ui.js";
-import { renderSplash, renderOnboard, renderHome, renderTraining, renderResults, renderPlayer, renderNav } from "./routes.js?v=861bbe14";
+import { renderSplash, renderOnboard, renderHome, renderTraining, renderResults, renderPlayer, renderNav } from "./routes.js?v=landing-logo-20260620";
 import { recommendedDrills } from "../data/drills.js";
 
 let state = normalizeState(loadState());
