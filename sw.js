e0e94a93c0796bbadd66702990cc690a3b5f2e6f
resiliencyhq-design <resiliencyhq@gmail.com>
2026-06-29 19:25:35 +1000

    Precache Step 2 lock stylesheet correctly

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const ASSETS=[
   "./index.html",
   "./css/style.css?v=onboarding-architecture-lock-20260628",
   "./css/onboard-step1-lock.css?v=step1-mvp-locked-20260628",
+  "./css/onboard-step2-lock.css?v=step2-lock-init-20260628",
   "./css/splash-fit.css?v=logo-down-20260628",
   "./css/onboarding-fix.css?v=hero-bg-top-20260623",
   "./css/academy-hero-asset.css?v=hero-bg-top-20260623",
@@ -47,6 +48,7 @@ self.addEventListener("fetch",e=>{
   }
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html"))));
 });
+
 
 
 
